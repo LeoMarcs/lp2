e41920457d5f24d2a3983a41f25eb7a455ff5c4b
LeoMarcs <leleo_vga@hotmail.com>
2015-03-23 12:46:06 -0300

    Create Exercicio1_4.java
    Ex4

---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,17 @@
+public class Exercicio1_4 {
+	public static void main(String [] args) {
+		float a, b, c, soma, mult, sub, division;
+		a = 45; 
+		b = 20;
+		c = 3;
+		soma = a + b + c;
+		sub = a - b -c; 
+		mult = a * b * c;
+		division = a/ b /c;
+		System.out.println("O valor da soma é: " + soma);
+		System.out.println("O valor da subtração é: " + sub);
+		System.out.println("O valor da multiplicação é: " + mult);
+		System.out.println("O valor da divisão é: " + division);
+
+	}
+}
